a7a4e18b2a429cfdbf7f8b7ef4385985888dc2ee
Naman <khandelwalnaman03@gmail.com>
2021-03-11 00:47:39 +0530

    added basic register page

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ff749f4574abef07a564b2d1'
 db = SQLAlchemy(app)
 
 
--- market/models.py
+++ market/models.py
@@ -1,6 +1,17 @@
 from market import db
 
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50),
+                              nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    # make relationship this shows our user can hold some items. Lazy = True will grab all items.
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -8,6 +19,7 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024),
                             nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
