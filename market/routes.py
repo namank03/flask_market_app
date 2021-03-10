5f05ab6f4d00e06a1e07766c09c746726c0b6bd5
Naman <khandelwalnaman03@gmail.com>
2021-03-11 01:57:14 +0530

    adding validator part1

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 
 @app.route('/')
@@ -16,7 +17,17 @@ def market_page():
     return render_template('market.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for error_msg in form.errors.values():
+            print(f'there was an error in creating user {error_msg}')
     return render_template('register.html', form=form)
