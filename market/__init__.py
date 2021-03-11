5b400e39bf5ea56b105e30e6a861dcd6c3598509
Naman <khandelwalnaman03@gmail.com>
2021-03-11 11:29:56 +0530

    configured for flask debugger

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,17 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
+
+# the toolbar is only enabled in debug mode:
+app.debug = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ff749f4574abef07a564b2d1'
-db = SQLAlchemy(app)
 
+
+db = SQLAlchemy(app)
+toolbar = DebugToolbarExtension(app)
 
 from market import routes
