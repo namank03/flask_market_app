8b65398b118c9971ec9cf343166a7b1935b0a997
Naman <khandelwalnaman03@gmail.com>
2021-03-11 11:00:04 +0530

    added unique user name and email filter

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError(
+                'This Username already exists in the database, Please try another one!')
+
+    def validate_email_address(self, email_address_to_check):
+        email = User.query.filter_by(
+            email_address=email_address_to_check.data).first()
+        if email:
+            raise ValidationError(
+                'This Email already exists in the database, Please try another one!')
+
     username = StringField(label="User Name:", validators=[
                            Length(min=2, max=30), DataRequired()])
     email_address = StringField(label="Email Address:", validators=[
